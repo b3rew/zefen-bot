29d30af85fa1804d2ff82fcb863decf167dc805c
berew <brkt13@gmail.com>
2017-03-13 17:07:16 +0300

    FIX #1
    changed configuration exports to CAPS
    switch from var to const and let
    change callback to promise
    change some of the code from imperative to declarative

--- index.js
+++ index.js
@@ -12,61 +12,65 @@ const musicApi = require('./api-parser');
 const config = require('./config');
 
 const options = {
-    polling: true
+  polling: true
 };
 const bot = new TelegramBot(TOKEN, options);
 
 // Matches /search [whatever]
 bot.onText(/\/search (.+)/, function onSearchText(msg, match) {
-    const keyword = match[1];
-    const opts = {
-        reply_to_message_id: msg.message_id,
-        parse_mode: 'HTML',
-    }
-    bot.sendChatAction(msg.chat.id, 'typing');
-    musicApi.getSongs('search', keyword, (songs) => {
-        bot.sendMessage(msg.chat.id, songs, opts);
-    })
+  const keyword = match[1];
+  const opts = {
+    reply_to_message_id: msg.message_id,
+    parse_mode: 'HTML',
+  }
+  bot.sendChatAction(msg.chat.id, 'typing');
+  musicApi.getSongs('search', keyword).then((songs) => {
+    bot.sendMessage(msg.chat.id, songs, opts);
+  })
 });
+
 // Matches /recent
 bot.onText(/\/recent/, function onRecent(msg) {
-    const opts = { parse_mode: 'HTML' }
-    bot.sendChatAction(msg.chat.id, 'typing');
-    musicApi.getSongs('recent', null, (songs) => {
-        bot.sendMessage(msg.chat.id, songs, opts);
-    })
+  const opts = { parse_mode: 'HTML' }
+  bot.sendChatAction(msg.chat.id, 'typing');
+  musicApi.getSongs('recent').then((songs) => {
+    bot.sendMessage(msg.chat.id, songs, opts);
+  })
 });
+
 // Matches /popular
 bot.onText(/\/popular/, function onPopular(msg) {
-    const opts = { parse_mode: 'HTML' }
-    bot.sendChatAction(msg.chat.id, 'typing');
-    musicApi.getSongs('popular', null, (songs) => {
-        bot.sendMessage(msg.chat.id, songs, opts);
-    })
+  const opts = { parse_mode: 'HTML' }
+  bot.sendChatAction(msg.chat.id, 'typing');
+  musicApi.getSongs('popular').then((songs) => {
+    bot.sendMessage(msg.chat.id, songs, opts);
+  })
 });
+
 // Matches /traditional
 bot.onText(/\/traditional/, function onTraditional(msg) {
-    const opts = { parse_mode: 'HTML' }
-    bot.sendChatAction(msg.chat.id, 'typing');
-    musicApi.getSongs('traditional', null, (songs) => {
-        bot.sendMessage(msg.chat.id, songs, opts);
-    })
+  const opts = { parse_mode: 'HTML' }
+  bot.sendChatAction(msg.chat.id, 'typing');
+  musicApi.getSongs('traditional').then((songs) => {
+    bot.sendMessage(msg.chat.id, songs, opts);
+  })
 });
+
 // Matches /random
 bot.onText(/\/random/, function onTraditional(msg) {
-    const opts = { parse_mode: 'HTML' }
-    bot.sendChatAction(msg.chat.id, 'typing');
-    musicApi.getSongs('random', null, (songs) => {
-        bot.sendMessage(msg.chat.id, songs, opts);
-    })
+  const opts = { parse_mode: 'HTML' }
+  bot.sendChatAction(msg.chat.id, 'typing');
+  musicApi.getSongs('random').then((songs) => {
+    bot.sendMessage(msg.chat.id, songs, opts);
+  })
 });
 
 // Matches /send play(and music id)
 bot.onText(/\/play(.+)/, function onPlay(msg, match) {
-    const resp = match[1];
-    const url = config.url.playSong + resp;
-    // console.log("url", url);
-    bot.sendChatAction(msg.chat.id, 'upload_audio');
-    //TODO: check url response before sending it, but now it is sending playSong.php.mp3 if the music is not found
-    bot.sendAudio(msg.chat.id, url);
+  const resp = match[1];
+  const url = config.URL.playSong + resp;
+  // console.log("url", url);
+  bot.sendChatAction(msg.chat.id, 'upload_audio');
+  //TODO: check url response before sending it, but now it is sending playSong.php.mp3 if the music is not found
+  bot.sendAudio(msg.chat.id, url);
 });
